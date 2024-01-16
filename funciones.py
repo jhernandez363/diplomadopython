db38db4d1c4a6c087bd3afcacb2f304b7f45f5d7
Jorge Hernandez <jorge980801@gmail.com>
2024-01-16 18:52:37 -0500

    Mis primeras versiones V1

---
+++ funciones.py
@@ -0,0 +1,19 @@
+import os
+os.system("cls")
+
+
+def suma (a,b):
+    c = a + b
+    print("La suma es a + b es: ",c)
+    
+def resta (a,b):
+    d = a + b
+    print("La resta de a - b es: ",d)
+    
+def multiplicacion (a,b):
+    pass
+def division (a,b):
+    pass
+
+suma(2,3)
+resta(2,3)
