2fccdc5e7a17affe439f01e8edc607f8e2caffe4
Jorge Hernandez <jorge980801@gmail.com>
2024-01-16 19:00:00 -0500

    Mi primera version V2

--- funciones.py
+++ funciones.py
@@ -7,13 +7,17 @@ def suma (a,b):
     print("La suma es a + b es: ",c)
     
 def resta (a,b):
-    d = a + b
+    d = a - b
     print("La resta de a - b es: ",d)
     
 def multiplicacion (a,b):
-    pass
+    e = a * b
+    print("La multiplicación de a * b es: ",e)
 def division (a,b):
-    pass
+    f = a / b
+    print("La división de a / b es: ",f)
 
 suma(2,3)
 resta(2,3)
+multiplicacion(2,3)
+division(2,3)
