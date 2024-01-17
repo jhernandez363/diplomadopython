e91a6b4cd16bb026e63dc8c9d4830cac3558326c
Jorge Hernandez <jorge980801@gmail.com>
2024-01-16 19:15:35 -0500

    Mi primera version V3

--- funciones.py
+++ funciones.py
@@ -14,10 +14,14 @@ def multiplicacion (a,b):
     e = a * b
     print("La multiplicación de a * b es: ",e)
 def division (a,b):
-    f = a / b
-    print("La división de a / b es: ",f)
+    if b == 0:
+        print("La división no se puede hacer")
+    else:
+        f = a/b
+        print("La division de a7b es: ",f)
+
 
 suma(2,3)
 resta(2,3)
 multiplicacion(2,3)
-division(2,3)
+division(2,0)
